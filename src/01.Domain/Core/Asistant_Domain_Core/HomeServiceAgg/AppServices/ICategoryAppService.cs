646ce535b4fd78b56a605482f2e3733d37da9e4d
behnazbayatloo <bayatloo.behnaz@gmail.com>
2026-01-30 13:11:44 +0330

    crud in home service and category

--- src/01.Domain/Core/Asistant_Domain_Core/HomeServiceAgg/AppServices/ICategoryAppService.cs
+++ src/01.Domain/Core/Asistant_Domain_Core/HomeServiceAgg/AppServices/ICategoryAppService.cs
@@ -1,4 +1,5 @@
-﻿using Asistant_Domain_Core.HomeServiceAgg.DTOs;
+﻿using Asistant_Domain_Core._commonEntities;
+using Asistant_Domain_Core.HomeServiceAgg.DTOs;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,11 @@ namespace Asistant_Domain_Core.HomeServiceAgg.AppServices
 {
     public interface ICategoryAppService
     {
+        Task<Result<bool>> CreateCategory(InputCategoryDTO categoryDTO, CancellationToken ct);
+        Task<bool> DeleteCategory(int id, CancellationToken ct);
         Task<IEnumerable<GetCategoryDTO>> GetAllCategories(CancellationToken ct);
+        Task<GetCategoryDTO?> GetCategoryById(int id, CancellationToken ct);
+        Task<PagedResult<GetCategoryDTO>> GetPagedCategories(int pageNumber, int pageSize, CancellationToken ct);
+        Task<Result<bool>> UpdateCategory(InputCategoryDTO categoryDTO, CancellationToken ct);
     }
 }
--- src/01.Domain/Core/Asistant_Domain_Core/HomeServiceAgg/AppServices/IHomeServiceAppService.cs
+++ src/01.Domain/Core/Asistant_Domain_Core/HomeServiceAgg/AppServices/IHomeServiceAppService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Asistant_Domain_Core._commonEntities;
+using Asistant_Domain_Core.HomeServiceAgg.DTOs;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,12 @@ namespace Asistant_Domain_Core.HomeServiceAgg.AppServices
 {
     public interface IHomeServiceAppService
     {
+        Task<Result<bool>> CreateHomeService(InputHomeServiceDTO homeServiceDTO, CancellationToken ct);
+        Task<bool> DeleteHomeService(int id, CancellationToken ct);
+        Task<IEnumerable<GetHomeServiceDTO>> GetAllHomeServices(CancellationToken ct);
+        Task<IEnumerable<GetHomeServiceDTO>> GetHomeServiceByCategoryId(int categoryId, CancellationToken ct);
+        Task<GetHomeServiceDTO?> GetHomeServiceById(int id, CancellationToken ct);
+        Task<PagedResult<GetHomeServiceDTO>> GetPagedHomeServices(int pageNumber, int pageSize, CancellationToken ct);
+        Task<Result<bool>> UpdateHomeService(InputHomeServiceDTO homeServiceDTO, CancellationToken ct);
     }
 }
