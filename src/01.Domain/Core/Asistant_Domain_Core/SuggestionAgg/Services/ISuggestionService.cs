ae4307dc769a516d3c79b576d283518afb3c61cb
behnazbayatloo <bayatloo.behnaz@gmail.com>
2026-01-30 13:13:38 +0330

    upgrade suggestion entity

--- src/01.Domain/Core/Asistant_Domain_Core/SuggestionAgg/Services/ISuggestionService.cs
+++ src/01.Domain/Core/Asistant_Domain_Core/SuggestionAgg/Services/ISuggestionService.cs
@@ -8,5 +8,7 @@ namespace Asistant_Domain_Core.SuggestionAgg.Services
 {
     public interface ISuggestionService
     {
+        Task<bool> DeleteSuggestionByCustomerId(CancellationToken ct, int id);
+        Task<bool> DeleteSuggestionByExpertId(CancellationToken ct, int id);
     }
 }
