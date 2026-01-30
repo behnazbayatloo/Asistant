8ac5434bb3550ef04266a07b6fe478255725b91f
behnazbayatloo <bayatloo.behnaz@gmail.com>
2026-01-30 13:06:45 +0330

    Customer Panel

---
+++ src/03.Presentasion/Asistant/Areas/Customer/Controllers/ProfileController.cs
@@ -0,0 +1,143 @@
+﻿using Asistant.Areas.Customer.Models;
+using Asistant_Domain_Core.UserAgg.AppServices;
+using Asistant_Domain_Core.UserAgg.DTOs;
+using Asistant_Domain_Core.UserAgg.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Threading.Tasks;
+
+namespace Asistant.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    [Authorize(Roles = "Customer")]
+    public class ProfileController(ILogger<ProfileController> logger, UserManager<AppUser> _userManager,
+        ICustomerAppService customerApp,ICityAppService cityApp) : Controller
+    {
+        public async Task<IActionResult> Index(CancellationToken ct)
+        {
+            var userId = Int32.Parse(_userManager.GetUserId(User));
+           
+            var model = new CustomerViewModel();
+            if (userId >0)
+            {
+                var customer = await customerApp.GetCustomerByUserId(userId, ct);
+                model = new CustomerViewModel
+                {
+                    Id = customer.Id,
+                    UserId = userId,
+                    Adress = customer.Adress,
+                    CityId = customer.CityId,
+                    CityName = customer.CityName,
+                    Email = customer.Email,
+                    FirstName = customer.FirstName,
+                    ImagePath = customer.ImagePath
+                    ,
+                    LastName = customer.LastName
+
+                };
+            }
+           
+            return View(model);
+        }
+
+        public  async Task<IActionResult> EditProfile(CancellationToken ct)
+        {
+            var cities = (await cityApp.GetAllCities(ct)).Select(c => new SelectListItem
+            {
+                Value = c.Id.ToString()
+                    ,
+                Text = c.Name
+            }).ToList();
+            cities.Insert(0, new SelectListItem { Value = "", Text = "یک شهر انتخاب کنید" });
+            var userId = Int32.Parse(_userManager.GetUserId(User));
+            var model = new CustomerViewModel();
+            if (userId >0)
+            {
+                var customer = await customerApp.GetCustomerByUserId(userId, ct);
+                model = new CustomerViewModel
+                {
+                    Id = customer.Id,
+                    UserId = userId,
+                    Adress = customer.Adress,
+                    CityId = customer.CityId,
+                    CityName = customer.CityName,
+                    Email = customer.Email,
+                    FirstName = customer.FirstName,
+                    ImagePath = customer.ImagePath
+                    ,
+                    LastName = customer.LastName
+
+                };
+            }
+            return View(model);
+        }
+        [HttpPost]
+        public async Task<IActionResult> EditProfile(EditCustomerViewModel model,CancellationToken ct)
+        {
+            var cities = (await cityApp.GetAllCities(ct)).Select(c => new SelectListItem
+            {
+                Value = c.Id.ToString()
+                    ,
+                Text = c.Name
+            }).ToList();
+            model.Cities = cities;
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            var editCustomer = new UpdateCustomerDTO
+            {
+                Id = model.Id,
+                Adress = model.Adress,
+                CityId = model.CityId,
+                Email = model.Email
+                 ,
+                FirstName = model.FirstName
+                 ,
+                LastName = model.LastName,
+                Password = model.Password,
+                UserId = model.UserId,
+                Image = model.Image
+            };
+            var result = await customerApp.UpdateCustomer(ct, editCustomer);
+            if (result)
+            {
+                ViewBag.Succeed = "تغییرات با موفقیت ذخیره شد";
+
+
+
+            }
+            else
+            {
+                ViewBag.Error = "تغییرات ثبت نشد";
+
+
+
+                ModelState.AddModelError("", "ویرایش ناموفق بود");
+
+            }
+
+            return View(model);
+        }
+        [HttpPost]
+        public async Task<IActionResult> DeleteProfile(int id,CancellationToken ct)
+        {
+            var userId = Int32.Parse(_userManager.GetUserId(User));
+             var result = await customerApp.DeleteCustomer(ct, id, userId);
+            if (result)
+
+            {
+                return RedirectToAction("Index", new {area =""});
+            }
+            else
+            {
+                TempData["Error"] = "عملیات موفقیت آمیز نبود";
+
+            }
+            return RedirectToAction("Index");
+
+        }
+    }
+}
---
+++ src/03.Presentasion/Asistant/Areas/Customer/Models/CustomerViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Asistant.Areas.Customer.Models
+{
+    public class CustomerViewModel
+    {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string Email { get; set; }
+        public string? Adress { get; set; }
+        public int UserId { get; set; }
+        public string? CityName { get; set; }
+        public int? CityId { get; set; }
+        public string? ImagePath { get; set; }
+    }
+}
