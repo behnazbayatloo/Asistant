a4f9574006bf5e71d5d0f560b3c0446f3af3ee15
behnazbayatloo <bayatloo.behnaz@gmail.com>
2026-01-30 13:14:30 +0330

    add common entities

---
+++ src/01.Domain/Core/Asistant_Domain_Core/_commonEntities/LoginResult.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asistant_Domain_Core._commonEntities
+{
+    public class LoginResult
+    {
+        public bool Result { get; set; }
+        public string? Message { get; set; }
+        public string? Role { get; set; }
+        public bool RequiresTwoFactor { get; set; }
+        public bool IsLockedOut { get; set; }
+    }
+}
---
+++ src/01.Domain/Core/Asistant_Domain_Core/_commonEntities/Result.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asistant_Domain_Core._commonEntities
+{
+    public class Result<T>
+    {
+        
+        public bool IsSuccess { get; }
+        public bool IsFailure => !IsSuccess;
+        public T Value { get; }
+        public string Message { get; }
+        protected Result(bool isSuccess, T value, string message) 
+        { IsSuccess = isSuccess; Value = value; Message = message; }
+        public static Result<T> Success(T value,string message) => new Result<T>(true,value, message);
+        public static Result<T> Failure(string error) => new Result<T>(false, default, error);
+    }
+}
