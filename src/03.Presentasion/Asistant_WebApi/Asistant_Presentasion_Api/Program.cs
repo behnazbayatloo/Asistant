b6e9742dd68ddb6ec72ad1ca7a64628c29a4be76
behnazbayatloo <bayatloo.behnaz@gmail.com>
2026-02-08 00:24:17 +0330

    Add Sitesetting section for WebApi

--- src/03.Presentasion/Asistant_WebApi/Asistant_Presentasion_Api/Program.cs
+++ src/03.Presentasion/Asistant_WebApi/Asistant_Presentasion_Api/Program.cs
@@ -2,12 +2,14 @@ using Asistant_Domain_AppService;
 using Asistant_Domain_Core.CommentAgg.AppService;
 using Asistant_Domain_Core.CommentAgg.Data;
 using Asistant_Domain_Core.CommentAgg.Service;
+using Asistant_Domain_Core.Configurations;
 using Asistant_Domain_Core.HomeServiceAgg.AppServices;
 using Asistant_Domain_Core.HomeServiceAgg.Data;
 using Asistant_Domain_Core.HomeServiceAgg.Services;
 using Asistant_Domain_Core.ImageAgg.AppService;
 using Asistant_Domain_Core.ImageAgg.Data;
 using Asistant_Domain_Core.ImageAgg.Service;
+using Asistant_Domain_Core.InfraContracts;
 using Asistant_Domain_Core.RequestAgg.AppServices;
 using Asistant_Domain_Core.RequestAgg.Data;
 using Asistant_Domain_Core.RequestAgg.Services;
@@ -19,10 +21,8 @@ using Asistant_Domain_Core.UserAgg.Data;
 using Asistant_Domain_Core.UserAgg.Entities;
 using Asistant_Domain_Core.UserAgg.Services;
 using Asistant_Domain_Service;
-
 using Asistant_Infra_Cache.InMemoryCache;
 using Asistant_Infra_Db_Sql.DbContext;
-
 using Asistant_Infra_File.Service;
 using Asistant_Infra_Repository.CommentAgg;
 using Asistant_Infra_Repository.HomeServiceAgg;
@@ -32,9 +32,8 @@ using Asistant_Infra_Repository.SuggestionAgg;
 using Asistant_Infra_Repository.UserAgg;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.OpenApi.Models;
 using Microsoft.Extensions.Caching.StackExchangeRedis;
-using Asistant_Domain_Core.InfraContracts;
+using Microsoft.OpenApi.Models;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -74,7 +73,10 @@ builder.Services.AddScoped<IAppUserRepository, AppUserRepository>();
 builder.Services.AddScoped<IAppUserService, AppUserService>();
 builder.Services.AddScoped<IAppUserAppService, AppUserAppService>();
 builder.Services.AddScoped<IFileService, FileService>();
+var siteSettings =
+    builder.Configuration.GetSection("SiteSettings").Get<SiteSettings>();
 
+builder.Services.AddSingleton(siteSettings);
 #endregion
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
@@ -96,7 +98,8 @@ if (app.Environment.IsDevelopment())
 {
    
     app.MapOpenApi();
-    app.UseSwaggerUI(options => { options.SwaggerEndpoint("/openapi/v1.json", "My API V1"); });
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
